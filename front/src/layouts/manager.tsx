aaad33260a3b60d9ba8633bac5131501a0234ad4
jerry <jerry@mg-consulting-it-act.com>
2025-08-24 15:29:06 +0300

    feat: implement sections for homepage

--- front/src/layouts/manager.tsx
+++ front/src/layouts/manager.tsx
@@ -1,17 +1,25 @@
+import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import ManagerToolbar from '../components/layouts/manager/Toolbar';
 import ManagerSidebar from '../components/layouts/manager/Sidebar';
 
 const ManagerLayout = () => {
+    const [visible, setVisible] = useState(false);
+
     return (
         <div className='flex'>
             <div className='w-[22vw]'>
-                <ManagerSidebar/>
+                <ManagerSidebar
+                    visible={visible}
+                    setVisible={setVisible}
+                />
             </div>
 
             <div className='w-full'>
                 <header>
-                    <ManagerToolbar />
+                    <ManagerToolbar 
+                        setVisible={setVisible}
+                    />
                 </header>
 
                 <main className='px-6 mt-4'>
